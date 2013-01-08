184d64b011ce78a59ebdfbec3bf4397d39f92c44
Frank Carver <frank.carver@googlemail.com>
2013-01-08 23:41:03 +0000

    some reformatting to fit better on a blog page

--- src/raspi/gpio.c
+++ src/raspi/gpio.c
@@ -8,27 +8,27 @@
 #define OKLED (1<<16)
 
 void raspi_okled_init() {
-	uint32_t sel = GET32(GPFSEL1);
-	sel &= ~(0b111 << 18);
-	sel |= (0b001 << 18);
-	PUT32(GPFSEL1,sel);
+  uint32_t sel = GET32(GPFSEL1);
+  sel &= ~(0b111 << 18);
+  sel |= (0b001 << 18);
+  PUT32(GPFSEL1,sel);
 }
 
 void raspi_okled_set(int state) {
-	if (state) {
-		PUT32(GPCLR0, OKLED);
-	} else {
-		PUT32(GPSET0, OKLED);
-	}
+  if (state) {
+    PUT32(GPCLR0, OKLED);
+  } else {
+    PUT32(GPSET0, OKLED);
+  }
 }
 
 void raspi_timer_wait(int usec) {
-	uint32_t start = GET32(COUNTER);
-	uint32_t end = start + usec;
-	while (GET32(COUNTER) < end)
-		;
+  uint32_t start = GET32(COUNTER);
+  uint32_t end = start + usec;
+  while (GET32(COUNTER) < end)
+    ;
 }
 
 void gpio_init() {
-	raspi_okled_init();
+  raspi_okled_init();
 }
